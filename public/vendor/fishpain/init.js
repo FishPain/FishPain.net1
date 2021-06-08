2978fa14ecea63ec270489e3e763e53e25f3a9bb
Tony Yu <itsfishpain@gmail.com>
2021-06-08 21:40:09 +0800

    update

--- public/vendor/fishpain/init.js
+++ public/vendor/fishpain/init.js
@@ -18,9 +18,7 @@ $(document).on('ready', function () {
         Turbolinks.start()
     });
 });
-console.log("%c---------------------------", 'font-size: 18px;text-decoration: line-through;')
-console.log('%cIchiharu', 'font-size: 60px; font-weight: bold; font-family: IBM Plex Sans; color: white')
-console.log('%cWARNING! ご注意！', 'font-size: 36px; font-weight: bold; font-family: Inter; color: #f4835d');
-console.log("%cPasting or typing anything here can expose your account details to hackers.\n\nここに何かを貼り付けたり、入力したりすると、ハッカーにアカウントの詳細を知られてしまう可能性があります。", 'font-size: 18px; font-family: Inter; font-weight: bold; color: red');
-console.log("%cIf someone is asking you to input anything here, they are 101% scamming you.", 'font-size: 18px; font-weight: regular; font-family: Inter; color: lightgrey');
-console.log("%c---------------------------", 'font-size: 18px;text-decoration: line-through;')
+console.log("")
+console.log("%cIf you see this, it's most likely that you know what you are doing.", 'font-size: 24px; font-weight: bold; font-family: IBM Plex Sans; color: black;');
+console.log("%cBut there's nothing to see here, so go away.\n ヘ(゜Д、゜)ノ", 'font-size: 24px; font-family: IBM Plex Sans; font-weight: regular; color: black;');
+console.log("")
